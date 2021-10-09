380c37f2be83c447d5330a6e91cb62fd9f915d21
Bethsyf <bethsyfalcon@gmail.com>
2021-10-09 07:22:22 -0500

    agregar utilidad nodemon

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ import Express from 'express';
 
 const app = Express();
 
-app.get('/products',(req, res) => {
+app.get('/products', (req, res) => {
     console.log('alguien hizo get en la ruta /products');
     res.send('productos no hay');
 });
 
-app.listen(5000,() => {
+app.listen(5000, () => {
     console.log('escuchando puerto 5000');
 });
