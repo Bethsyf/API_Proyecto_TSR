1895d0c3cc6666c02d24e024fcb889780e0197de
milecafa <milena.cafa@gmail.com>
2021-10-22 20:28:52 -0500

    jason web token

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import { conectarBD } from './db/db.js';
 import rutasProducto from './views/products/rutas.js';
 import rutasUsuario from './views/users/rutas.js';
 import rutasVenta from './views/sales/rutas.js';
+import jwt from 'express-jwt';
+import jwks from 'jwks-rsa';
 
 dotenv.config({ path: './.env' });
 
@@ -12,6 +14,21 @@ const app = Express();
 
 app.use(Express.json());
 app.use(Cors());
+
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: 'https://misiontic-tsr.us.auth0.com/.well-known/jwks.json'
+}),
+audience: 'api-autenticacion-trs',
+issuer: 'https://misiontic-tsr.us.auth0.com/',
+algorithms: ['RS256']
+});
+
+app.use(jwtCheck);
+
 app.use(rutasProducto);
 app.use(rutasUsuario);
 app.use(rutasVenta);
